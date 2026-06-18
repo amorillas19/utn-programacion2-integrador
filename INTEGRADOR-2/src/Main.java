aef9ec5fd3843d0a27fe6606d51681437857cfc1
Andres Morillas <patomorillas@gmail.com>
2026-06-18 14:40:44 -0300

    estructura inicial menu

--- INTEGRADOR-2/src/Main.java
+++ INTEGRADOR-2/src/Main.java
@@ -1,15 +1,20 @@
 import entities.*;
 import enums.*;
+import ui.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Categoria c1 = new Categoria("Lacteos", "vaca y vegetales");
+        /*Categoria c1 = new Categoria("Lacteos", "vaca y vegetales");
         Categoria c2 = new Categoria("Panaderia", "panes y facturas");
         
         Producto pro1 = new Producto("Pan", 250.99, "Casero", 10, "pan.png", true);
         Producto pro2 = new Producto("Leche", 399.99, "Almendra", 15, "lechealmendra.png", true);
         System.out.println(c1);
-        System.out.println(pro1);
+        System.out.println(pro1);*/
+
+        Menu menu = new Menu();
+        menu.mostrarMenu();
+
         
     }
 }
